0ab898e7f7fefe86487dbb4c46fb33f8d0a123d4
Ashwin-A-K <ashwinkumar3098@gmail.com>
2018-11-14 09:04:24 -0600

    testing

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,27 @@
 pragma solidity ^"0.4.2";
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
-
-    // Constructor
-    function Election () public {
-        candidate = "Candidate 1";
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
+
+    // Read/write candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
+
+    constructor() Election () public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
 }
